a0d2b1c34d7ed78fa6916e4c254366bd0f6de57b
panpingjuan <2325572828@qq.com>
2020-05-22 09:15:04 +0800

    oh

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args){
         System.out.println("Hi,World!");
-        System.out.println("oh my god")
+        System.out.println("oh my god");
+        System.out.println(("SO HAPPY."));
     }
 }
